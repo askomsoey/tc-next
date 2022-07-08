f94b958160e13f9b8c8d2d538e4b31e6f87dd254
askomsoey-yoobic <askomsoey@yoobic.com>
2022-07-08 07:47:02 +0100

    feat(molecules/header): add Buttons into header story

--- components/molecules/header/Header.stories.tsx
+++ components/molecules/header/Header.stories.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Header } from './Header';
+import { Button } from '../../atoms/button/Button';
 
 export default {
-  title: 'Example/Header',
+  title: 'Molucules/Header',
   component: Header,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
 } as ComponentMeta<typeof Header>;
@@ -18,9 +18,9 @@ export const LoggedIn: ComponentStory<typeof Header> = () => (
     center={<h2>Welcome Message</h2>}
     end={
       <div className='header-menu'>
-        <p>Home</p>
-        <p>About</p>
-        <p>Login</p>
+        <Button label='Home' type='transparent' />
+        <Button label='About' type='transparent' />
+        <Button label='Login' type='transparent' />
       </div>
     }
   />
