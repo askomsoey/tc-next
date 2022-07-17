07d3156c8f0696911ea5ed45591f235db2581102
askomsoey-yoobic <askomsoey@yoobic.com>
2022-07-17 20:21:49 +0100

    feat(config): set up base numbers mocker

--- utils/mocks/mocker/index.ts
+++ utils/mocks/mocker/index.ts
@@ -1,11 +1,12 @@
 import { files } from './files';
 import { media } from './media';
+import { numbers } from './numbers';
 import { text } from './text';
 
 export const Mocker = {
   media,
   files,
   text,
-  numbers: {},
+  numbers,
   entities: {},
 };
